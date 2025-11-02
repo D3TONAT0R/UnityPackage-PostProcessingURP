a48ce99f9ada2d053c524989211b16ccc4ce0016
D3TONAT0R <gdv@swissonline.ch>
2025-11-02 18:05:14 +0100

    XML documentation

--- Runtime/Renderer/CustomPostProcessVolumeComponent.cs
+++ Runtime/Renderer/CustomPostProcessVolumeComponent.cs
@@ -3,6 +3,10 @@ using UnityEngine.Rendering.RenderGraphModule;
 
 namespace UnityEngine.Rendering.Universal.PostProcessing
 {
+	/// <summary>
+	/// Represents a volume parameter that holds a <see cref="PostProcessingPassEvent"/> value.
+	/// </summary>
+	/// <remarks>This parameter is used to define when a post-processing effect occurs within a rendering pipeline.</remarks>
 	[System.Serializable]
 	public class PassEventParameter : VolumeParameter<PostProcessingPassEvent>
 	{
@@ -12,8 +16,14 @@ namespace UnityEngine.Rendering.Universal.PostProcessing
 		}
 	}
 
+	/// <summary>
+	/// The base class for custom post-processing volume components.
+	/// </summary>
 	public abstract class CustomPostProcessVolumeComponent : VolumeComponent, IPostProcessComponent
 	{
+		/// <summary>
+		/// A container for data used during the rendering of a post-processing pass.
+		/// </summary>
 		public class PassData
 		{
 			public TextureHandle source;
@@ -23,24 +33,53 @@ namespace UnityEngine.Rendering.Universal.PostProcessing
 
 		//These Will be drawn manually with a custom editor
 
+		/// <summary>
+		/// Whether this post-processing effect is enabled.
+		/// </summary>
 		[HideInInspector]
 		public BoolParameter enabled = new BoolParameter(false);
 		//Only visible if SupportsBlending is true
+		/// <summary>
+		/// The blending factor for this effect, ranging from 0 to 1. Only applicable if <see cref="SupportsBlending"/> is true."/>
+		/// </summary>
 		[HideInInspector]
 		public ClampedFloatParameter blend = new ClampedFloatParameter(0f, 0, 1, true);
 
+		/// <summary>
+		/// The name of the primary shader used for this post-processing effect.
+		/// </summary>
 		public abstract string ShaderName { get; }
 
+		/// <summary>
+		/// The injection point for this post-processing effect within the rendering pipeline.
+		/// </summary>
 		public abstract PostProcessingPassEvent InjectionPoint { get; }
 
+		/// <summary>
+		/// If true, the effect will be rendered even if post-processing is disabled for the camera.
+		/// </summary>
 		public virtual bool IgnorePostProcessingFlag => false;
 
+		/// <summary>
+		/// If true, the effect supports blending and will use the blend parameter.
+		/// </summary>
 		public virtual bool SupportsBlending => false;
 
+		/// <summary>
+		/// If true, the effect will be rendered in the scene view. To render all effects in the scene view regardless of this setting,
+		/// use the scene view "Post Processing" debug view.
+		/// </summary>
 		public virtual bool VisibleInSceneView => true;
 
+		//TODO: check requirements and request depth / normals if needed
+		/// <summary>
+		/// The input requirements for this post-processing effect.
+		/// </summary>
 		public virtual ScriptableRenderPassInput Requirements => ScriptableRenderPassInput.Color;
 
+		/// <summary>
+		/// The render pass event corresponding to the injection point of this effect.
+		/// </summary>
 		public RenderPassEvent RenderPassEvent
 		{
 			get
@@ -65,6 +104,7 @@ namespace UnityEngine.Rendering.Universal.PostProcessing
 
 		protected static readonly int blendPropertyId = Shader.PropertyToID("_Blend");
 
+		/// <inheritdoc/>
 		public virtual bool IsActive() {
 			if(!enabled.value)
 				return false;
@@ -73,8 +113,12 @@ namespace UnityEngine.Rendering.Universal.PostProcessing
 			return true;
 		}
 
+		/// <inheritdoc/>
 		public virtual bool IsTileCompatible() => true;
 
+		/// <summary>
+		/// Called before the effect is rendered for the first time.
+		/// </summary>
 		protected virtual void Initialize(CustomPostProcessPass pass)
 		{
 			typeName = GetType().Name;
@@ -94,6 +138,9 @@ namespace UnityEngine.Rendering.Universal.PostProcessing
 			initialized = true;
 		}
 
+		/// <summary>
+		/// Renders the post-processing effect, if enabled in the given context.
+		/// </summary>
 		public void Render(CustomPostProcessPass pass, RenderGraphModule.RenderGraph renderGraph,
 			UniversalResourceData frameData, ContextContainer context)
 		{
@@ -104,12 +151,18 @@ namespace UnityEngine.Rendering.Universal.PostProcessing
 			RenderEffect(pass, renderGraph, frameData, context);
 		}
 
+		/// <summary>
+		/// Performs the actual rendering of the effect. Unless overridden, it performs a blit using the effect's material.
+		/// </summary>
 		protected virtual void RenderEffect(CustomPostProcessPass pass, RenderGraphModule.RenderGraph renderGraph,
 			UniversalResourceData frameData, ContextContainer context)
 		{
 			Blit(renderGraph, frameData);
 		}
 
+		/// <summary>
+		/// Initializes the effect for rendering and checks if it should be rendered based on the camera context.
+		/// </summary>
 		protected bool BeginRender(CustomPostProcessPass pass, ContextContainer context)
 		{
 			var data = context.Get<UniversalCameraData>();
@@ -126,6 +179,14 @@ namespace UnityEngine.Rendering.Universal.PostProcessing
 			}
 			InitializeBlitMaterial();
 			return true;
+		}
+
+		/// <summary>
+		/// Sets material properties for the effect's material.
+		/// </summary>
+		public virtual void SetMaterialProperties(Material mat)
+		{
+
 		}
 
 		private bool CheckEffectVisibilityForCamera(UniversalCameraData data)
@@ -157,11 +218,9 @@ namespace UnityEngine.Rendering.Universal.PostProcessing
 			SetMaterialProperties(blitMaterial);
 		}
 
-		public virtual void SetMaterialProperties(Material mat)
-		{
-
-		}
-
+		/// <summary>
+		/// Performs a blit operation using the effect's material.
+		/// </summary>
 		protected void Blit(RenderGraphModule.RenderGraph renderGraph, UniversalResourceData frameData, int passIndex = 0)
 		{
 			using(var builder = renderGraph.AddRasterRenderPass<PassData>(typeName, out var passData))
@@ -187,6 +246,9 @@ namespace UnityEngine.Rendering.Universal.PostProcessing
 			}
 		}
 
+		/// <summary>
+		/// The execution function for the raster render pass that performs the blit operation.
+		/// </summary>
 		protected virtual void ExecuteRasterRenderPass(PassData data, RasterGraphContext context)
 		{
 			Blitter.BlitTexture(context.cmd, data.source, new Vector4(1, 1, 0, 0), data.material, data.passIndex);
